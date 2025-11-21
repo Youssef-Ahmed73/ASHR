bb2f6048711e108513b78cd94bc9222d43d08f49
Youssef Ahmed <Youssef.nagy073@gmail.com>
2025-11-21 20:05:04 +0200

    moveit base configuration

---
+++ build/moveit_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/youssef/strawberry_ws/install/myrobot/share/myrobot/package.sh"
---
+++ build/myrobot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/youssef/strawberry_ws/src/myrobot
+# Build directory: /home/youssef/strawberry_ws/build/myrobot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
